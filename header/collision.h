a7e765d50868f87795b9e65d769ed4998be026a1
Jerome Joseph <jejoweb2020@gmail.com>
2023-06-04 18:47:29 +0530

    Added Collision Detection for comets

--- header/collision.h
+++ header/collision.h
@@ -2,6 +2,6 @@
 #define COLLISION_H
 
 void check_ship_bullet_collision(bullet* bullets[MAX_BULLETS], obj* enemy_ships[MAX_ENEMY_SHIPS], comet* comets[MAX_COMETS]);
-void check_enemy_bullet_collision(obj* ship, bullet* enemy_bullets[MAX_ENEMY_BULLETS]);
+void check_enemy_bullet_collision(obj* ship, bullet* enemy_bullets[MAX_ENEMY_BULLETS], comet* comets[MAX_COMETS]);
 
 #endif 
